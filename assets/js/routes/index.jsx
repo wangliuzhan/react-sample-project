cc0d3819a0e326e820354b42b0ecf7c971b5f4f7
simongfxu <xugaofan@gmail.com>
2015-11-26 17:33:40 +0800

    add map2Props.jsx

--- assets/js/routes/index.jsx
+++ assets/js/routes/index.jsx
@@ -8,41 +8,7 @@ import {connect} from 'react-redux'
 import Home from '../pages/home.jsx'
 import RealTime from '../pages/realtime.jsx'
 import Event from '../pages/event.jsx'
-import GameActionCreator from '../actions/game.jsx'
-import _ from 'lodash'
-
-// 每个action对应自己的模块名称
-let actionCreatorList = {
-  game: GameActionCreator
-}
-
-/**
- * 状态数据都存储在states属性
- */
-function mapStateToProps(state = {}) {
-  return {
-    states: Object.assign({}, state)
-  }
-}
-
-/**
- * 最终传递给组件props的actions形式如下：
- * onGameCreate => 对应actions/game.jsx (create)
- */
-function mapDispatchToProps(dispatch) {
-  let actions = {}
-  Object.keys(actionCreatorList).forEach((moduleName) => {
-    let creator = actionCreatorList[moduleName]
-    Object.keys(creator).forEach((action) => {
-      let realActionName = _.camelCase(['on', moduleName, action].join('_'))
-      actions[realActionName] = (data) => {
-        dispatch(GameActionCreator[action](data))
-      }
-    })
-  })
-
-  return {actions}
-}
+import {mapStateToProps, mapDispatchToProps} from '../helpers/map2Props.jsx'
 
 let Entry = connect(
   mapStateToProps,
