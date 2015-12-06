f971badef9aca7fdd05a18dee82fde83f8bddb4a
simongfxu <xugaofan@gmail.com>
2015-12-06 17:50:23 +0800

    delete mock/*

--- assets/js/mocks/index.js
+++ assets/js/mocks/index.js
@@ -29,11 +29,28 @@ const mock = App.useMock ? mocker(request) : {
 //   };
 // });
 
-mock.post('/abc', function(req) {
+mock.get('/mock/retain2.json', function(req) {
   return {
-    id: 1,
     body: {
-      a: 1
+      "statusCode": 200,
+      "name": {
+        "y0": "付费次数",
+        "y1": "占比"
+      },
+      "content": [{
+        "y1": 0.5357142857142857,
+        "y0": 1500,
+        "x": "1"
+      }, {
+        "y1": 0.25,
+        "y0": 700,
+        "x": "4"
+      }, {
+        "y1": 0.21428571428571427,
+        "y0": 600,
+        "x": "2-3"
+      }],
+      "id": "1448090145560J"
     }
   }
 })
